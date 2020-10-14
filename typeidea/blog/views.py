dbee41c92e6ee5a8d4f10fdb55d47616a2a5d7f9
ganggu <ganggu@newborntown.com>
2020-10-14 17:45:58 +0800

    完成base_html抽离

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 
 from config.models import SideBar
 from .models import Tag, Post, Category
